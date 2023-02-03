32b2e3aaedd8f4330d55fb9f6ecc53ffe0fa6950
Silke <silk2918@stud.kea.dk>
2023-02-03 15:08:36 +0100

    force string in input, so the upper() method gets called

--- api/main.py
+++ api/main.py
@@ -32,7 +32,7 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: str):
     name = q.upper()
     return {"item_id": item_id, "q": name}
 
