92fbd20946d100d03462e9267f5a93f9872266cf
Silke <silk2918@stud.kea.dk>
2023-02-03 11:04:29 +0100

    add comments

--- tests/tests.py
+++ tests/tests.py
@@ -1,20 +1,36 @@
 from starlette.testclient import TestClient
-
 from api.main import app
 
 client = TestClient(app)
 
-def test_root_endpoint():
+
+def test_read_root():
+
+    #when
     res = client.get("/")
+    
+    #then
     assert res.status_code == 200
     assert res.json() == {"Hello": "World"}
 
-def test_positive_id():
+def test_create_student_positive_id():
+
+    #given
     json_blob = {"student_id": 1, "name": "Jens"}
+
+    #when
     res = client.post("/students/", json=json_blob)
+
+    #then
     assert res.status_code == 200
 
-def test_negative_id():
+def test_create_student_negative_id():
+
+    #given
     json_blob = {"student_id": -1, "name": "Jens"}
+    
+    #when
     res = client.post("/students/", json=json_blob)
+    
+    #then
     assert res.status_code != 200
