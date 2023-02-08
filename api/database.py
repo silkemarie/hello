a85756d4fae319b1c1f828c03f98b8ee28fad022
Silke <silk2918@stud.kea.dk>
2023-02-08 13:58:20 +0100

    use python-decouple to get .env

--- api/database.py
+++ api/database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from decouple import config
 
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_api.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/hello"
-#SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/hello"
+SQLALCHEMY_DATABASE_URL = config("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
