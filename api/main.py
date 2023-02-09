053334ce433e4afec70d45ea3170e48dc955e354
Silke <silk2918@stud.kea.dk>
2023-02-10 00:38:20 +0100

    users/me works with db

--- api/main.py
+++ api/main.py
@@ -54,7 +54,7 @@ def fake_hash_password(password: str):
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-class User(BaseModel):
+""" class User(BaseModel):
     username: str
     email: Union[str, None] = None
     full_name: Union[str, None] = None
@@ -62,7 +62,7 @@ class User(BaseModel):
 
 
 class UserInDB(User):
-    hashed_password: str
+    hashed_password: str """
 
 
 def get_user(db, username: str):
@@ -87,26 +87,28 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
     return user
 
 
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
+async def get_current_active_user(current_user: schemas.User = Depends(get_current_user)):
+    """ if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user") """
     return current_user
 
 
 @app.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = get_user(db, form_data.email)
+    user = get_user(db, form_data.username)
+   
+    """  print(form_data.password)
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     hashed_password = fake_hash_password(form_data.password)
     if not hashed_password == user.hashed_password:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
-
+    """
     return {"access_token": user.username, "token_type": "bearer"}
 
 
 @app.get("/users/me")
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
+async def read_users_me(current_user: schemas.User = Depends(get_current_active_user)):
     return current_user
 
 # End of Auth
